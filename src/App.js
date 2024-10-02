8a4118a84c4e339bf508666e21fa1c9a513ff93d
ALFAFA <gasan1288@gmail.com>
2024-10-02 19:07:09 +0400

    Create and style footer section component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Intro from "./component/Intro/intro";
 import Skills from "./component/Skills/skills";
 import Works from "./component/Works/works";
 import Contact from "./component/Contact/contact";
+import Footer from "./component/Footer/footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Skills />
       <Works />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/component/Footer/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './footer.css';
+export default function Footer() {
+    return (
+        <footer className="footer">
+            Copyright &#169; 2024 Gasan Omer. All right received.
+        </footer>
+    );
+}
