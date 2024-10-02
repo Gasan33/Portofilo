d647fb82957741a785372c3b45175b1e38b76e8f
ALFAFA <gasan1288@gmail.com>
2024-10-02 20:22:27 +0400

    Add auto send email service

--- src/component/Contact/contact.js
+++ src/component/Contact/contact.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser';
 import './contact.css';
 import facebook from "../../assets/facebook.png";
 import facebookIcon from "../../assets/facebookIcon.png";
 export default function Contact() {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_mz9l2m9', 'template_d84h25s', form.current, {
+                publicKey: 'ngmsIeCWgJWFGFdV6',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                    e.target.reset();
+                    alert('Email Sent ! we will contact you soon.')
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
     return (
         <section id="contactPage">
             {/* <div id="clients">
@@ -22,9 +42,9 @@ export default function Contact() {
             <div id="contact">
                 <h1 className="contactPageTitle">Contact Me</h1>
                 <span className="contactDesc">Please fill out the form elow to discuss any work opportunities.</span>
-                <form className="contactForm">
-                    <input type="text" className="name" placeholder="Your Name" />
-                    <input type="email" className="email" placeholder="Your E-mail" />
+                <form className="contactForm" ref={form} onSubmit={sendEmail}>
+                    <input type="text" className="name" placeholder="Your Name" name="from_name" />
+                    <input type="email" className="email" placeholder="Your E-mail" name="your_email" />
                     <textarea className="msg" name="message" rows="5" placeholder="Your Message"></textarea>
                     <button type="submit" value="send" className="submitBtn">Submit</button>
                     <div className="links">
